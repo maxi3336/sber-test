74e02864ec895fc7d77b6902d8c69376ee9a1db4
Max <max.book@list.ru>
2021-04-07 11:13:16 +0500

    Create Keep wip

--- src/components/Keep/ToDoItem.tsx
+++ src/components/Keep/ToDoItem.tsx
@@ -5,22 +5,37 @@ import { ReactComponent as CheckedBox } from "../../assets/icons/check-box.svg";
 import { ReactComponent as DeleteIcon } from "../../assets/icons/delete.svg";
 
 type Props = {
-  onClick: () => void;
-  deleteHandler: () => void;
-  isChecked: boolean;
+  onClick: (id: string) => void;
+  deleteHandler: (id: string) => void;
   canBeDeleted: boolean;
-  content: string;
+  todo: {
+    id: string;
+    isChecked: boolean;
+    content: string;
+  };
 };
 
 export const ToDoItem = (props: Props) => {
   return (
     <div className="todo">
-      <div className="todo__checkbox" onClick={props.onClick}>
-        {!props.isChecked ? <UncheckedBox /> : <CheckedBox />}
+      <div
+        className="todo__checkbox"
+        onClick={() => props.onClick(props.todo.id)}
+      >
+        {!props.todo.isChecked ? <UncheckedBox /> : <CheckedBox />}
       </div>
-      <div className="todo__content">{props.content}</div>
+      <div
+        className={
+          props.todo.isChecked ? "todo__content--checked" : "todo__content"
+        }
+      >
+        {props.todo.content}
+      </div>
       {props.canBeDeleted ? (
-        <div className="todo__delete" onClick={props.deleteHandler}>
+        <div
+          className="todo__delete"
+          onClick={() => props.deleteHandler(props.todo.id)}
+        >
           <DeleteIcon />
         </div>
       ) : null}
--- src/components/Modals/CreateKeepModal.tsx
+++ src/components/Modals/CreateKeepModal.tsx
@@ -3,8 +3,59 @@ import "../../styles/CreateKeepModal.scss";
 import { Portal } from "react-portal";
 import { ReactComponent as CloseIcon } from "../../assets/icons/close.svg";
 import { ToDoItem } from "../Keep/ToDoItem";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+type Todo = {
+  id: string;
+  content: string;
+  isChecked: boolean;
+};
+
+type Keep = {
+  title: string;
+  todos: Todo[];
+};
 
 export const CreateKeepModal = (props: { closeModal: () => void }) => {
+  const [keep, setKeep] = useState<Keep>();
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todoValue, setTodoValue] = useState("");
+
+  const todoValueHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  };
+
+  const addTodoHandler = () => {
+    setTodos((prev) => [
+      ...prev,
+      { id: uuidv4(), content: todoValue, isChecked: false },
+    ]);
+    setTodoValue("");
+  };
+
+  const checkTodoHandler = (id: string) => {
+    setTodos((prev) =>
+      prev.map((todo) => {
+        if (todo.id === id)
+          return {
+            isChecked: !todo.isChecked,
+            content: todo.content,
+            id: todo.id,
+          };
+        return todo;
+      })
+    );
+  };
+
+  const deleteTodoHandler = (id: string) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const submitHandler = () => {
+    console.log(todos);
+  };
+
   return (
     <Portal>
       <div className="create-modal base-animation">
@@ -22,51 +73,39 @@ export const CreateKeepModal = (props: { closeModal: () => void }) => {
               </div>
               <div className="create-modal__add-todo">
                 <div className="create-modal__input">
-                  <input placeholder="Add todo" />
+                  <input
+                    placeholder="Add todo"
+                    value={todoValue}
+                    onChange={todoValueHandler}
+                  />
                 </div>
-                <button>Add</button>
+                <button onClick={addTodoHandler}>Add</button>
               </div>
             </div>
             <div className="column todo-list">
               <h3>ToDo List:</h3>
-              <ToDoItem
-                onClick={() => {}}
-                isChecked={false}
-                content="Heasdfasdfasdfasdfasdfasdfsllo"
-                deleteHandler={() => {}}
-                canBeDeleted
-              />
-              <ToDoItem
-                onClick={() => {}}
-                isChecked={false}
-                content="Heasdfasdfasdfasdfasdfasdfsllo"
-                deleteHandler={() => {}}
-                canBeDeleted
-              />
-              <ToDoItem
-                onClick={() => {}}
-                isChecked={false}
-                content="Heasdfasdfasdfasdfasdfasdfsllo"
-                deleteHandler={() => {}}
-                canBeDeleted
-              />
-              <ToDoItem
-                onClick={() => {}}
-                isChecked={false}
-                content="Heasdfasdfasdfasdfasdfasdfsllo"
-                deleteHandler={() => {}}
-                canBeDeleted
-              />
-              <ToDoItem
-                onClick={() => {}}
-                isChecked={false}
-                content="Heasdfasdfasdfasdfasdfasdfsllo"
-                deleteHandler={() => {}}
-                canBeDeleted
-              />
+              {todos.map((todo) => {
+                if (!todo) return null;
+
+                return (
+                  <ToDoItem
+                    key={todo.id}
+                    onClick={checkTodoHandler}
+                    deleteHandler={deleteTodoHandler}
+                    canBeDeleted
+                    todo={{
+                      id: todo.id,
+                      isChecked: todo.isChecked,
+                      content: todo.content,
+                    }}
+                  />
+                );
+              })}
             </div>
           </div>
-          <button className="create-modal__submit">Create</button>
+          <button className="create-modal__submit" onClick={submitHandler}>
+            Create
+          </button>
         </div>
       </div>
     </Portal>
